f0a68efc7fbedf088f656ed22855f14cb372edd7
bednar@borek-laptop <jakub.bednar@gmail.com>
2013-10-20 10:50:44 +0200

    * Build Api Documentation

--- src/main/java/com/github/bednar/aap/mojo/GenerateMojo.java
+++ src/main/java/com/github/bednar/aap/mojo/GenerateMojo.java
@@ -1,16 +1,29 @@
 package com.github.bednar.aap.mojo;
 
+import javax.annotation.Nonnull;
+import javax.annotation.Nullable;
 import java.io.File;
-import java.io.IOException;
-import java.nio.charset.StandardCharsets;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.net.URLClassLoader;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.Collection;
+import java.util.List;
 
 import com.github.bednar.aap.processor.doc.Apiary;
-import com.google.common.io.Files;
+import com.google.common.base.Function;
+import com.google.common.base.Predicate;
+import com.google.common.collect.FluentIterable;
+import com.wordnik.swagger.annotations.Api;
+import com.wordnik.swagger.annotations.ApiModel;
 import org.apache.maven.plugin.AbstractMojo;
 import org.apache.maven.plugin.MojoExecutionException;
 import org.apache.maven.plugin.MojoFailureException;
 import org.apache.maven.plugins.annotations.Mojo;
 import org.apache.maven.plugins.annotations.Parameter;
+import org.reflections.Reflections;
+import org.reflections.util.ConfigurationBuilder;
 
 /**
  * @author Jakub Bednář (15/10/2013 4:24 PM)
@@ -18,6 +31,9 @@ import org.apache.maven.plugins.annotations.Parameter;
 @Mojo(name = "generate")
 public class GenerateMojo extends AbstractMojo
 {
+    @Parameter(defaultValue = "${project.compileClasspathElements}", required = true, readonly = true)
+    private List<String> sourceCompiledPaths;
+
     /**
      * Documentation output
      */
@@ -47,20 +63,60 @@ public class GenerateMojo extends AbstractMojo
     {
         getLog().info(String.format("[apiary-cfg][appName:%s][apiBaseURL:%s][appDescription:%s]", appName, apiBaseURL, appDescription));
 
-        if (appDescription.exists())
-        {
-            try
-            {
-                getLog().info(Files.toString(appDescription, StandardCharsets.UTF_8));
-            }
-            catch (IOException e)
-            {
-                getLog().error(e);
-            }
-        }
+        URLClassLoader urlClassLoader = buildClassPath();
+
+        Reflections reflections = new Reflections(
+                new ConfigurationBuilder()
+                        .setUrls(urlClassLoader.getURLs())
+                        .addClassLoader(urlClassLoader));
+
+        Collection<Class<?>> apis       = reflections.getTypesAnnotatedWith(Api.class);
+        Collection<Class<?>> entities   = reflections.getTypesAnnotatedWith(ApiModel.class);
 
         Apiary
                 .create(apiaryOutput)
+                .addApis(apis)
+                .addEntities(entities)
                 .generate(appName, apiBaseURL, appDescription);
+    }
+
+    @Nonnull
+    private URLClassLoader buildClassPath()
+    {
+        //Add exist compiled path
+        URL[] urls = FluentIterable.from(sourceCompiledPaths)
+                .filter(new Predicate<String>()
+                {
+                    @Override
+                    public boolean apply(@Nullable final String path)
+                    {
+                        return path != null && Files.exists(Paths.get(path));
+                    }
+                })
+                .transform(new Function<String, URL>()
+                {
+                    @Override
+                    public URL apply(@SuppressWarnings("NullableProblems") @Nonnull final String path)
+                    {
+                        try
+                        {
+                            return Paths.get(path).toUri().toURL();
+                        }
+                        catch (MalformedURLException e)
+                        {
+                            throw new GenerateMojoException(e);
+                        }
+                    }
+                }).toArray(URL.class);
+
+        return URLClassLoader.newInstance(urls, Thread.currentThread().getContextClassLoader());
+    }
+
+    private class GenerateMojoException extends RuntimeException
+    {
+        private GenerateMojoException(final @Nonnull Throwable cause)
+        {
+            super(cause);
+        }
     }
 }
