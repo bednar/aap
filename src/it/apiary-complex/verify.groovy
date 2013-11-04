58775687be89fbf3ec69065123ad8c1608e3b129
bednar@borek-laptop <jakub.bednar@gmail.com>
2013-11-04 17:04:06 +0100

    - Logging mess

--- src/it/apiary-complex/verify.groovy
+++ src/it/apiary-complex/verify.groovy
@@ -1,24 +1,7 @@
-import difflib.*
-
 apiaryExpected = new File(basedir, "ApiaryExpected.md")
 assert apiaryExpected.exists()
-
-println '\n Expected: '
-println apiaryExpected.getText()
 
 apiaryResult = new File(basedir, "target/generated/apiary/Apiary.md")
 assert apiaryResult.exists()
 
-println '\n Result: '
-println apiaryResult.getText()
-
-Patch patch = DiffUtils.diff(apiaryExpected.readLines(), apiaryResult.readLines())
-
-println patch.getDeltas().size()
-
-patch.getDeltas().each {
-    println it
-}
-
-assert patch.getDeltas().size() == 0
 assert apiaryExpected.getText().equals(apiaryResult.getText())
