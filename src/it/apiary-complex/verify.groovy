e1aa641c6aabd0bf4a27a5a8721501926317b7d3
bednar@borek-laptop <jakub.bednar@gmail.com>
2013-11-04 15:26:04 +0100

    * Compare Files by DiffUtils

--- src/it/apiary-complex/verify.groovy
+++ src/it/apiary-complex/verify.groovy
@@ -1,7 +1,17 @@
+import difflib.*
+
 apiaryExpected = new File(basedir, "ApiaryExpected.md")
 assert apiaryExpected.exists()
 
 apiaryResult = new File(basedir, "target/generated/apiary/Apiary.md")
 assert apiaryResult.exists()
 
-assert apiaryExpected.getText().equals(apiaryResult.getText())
+Patch patch = DiffUtils.diff(apiaryExpected.readLines(), apiaryResult.readLines())
+
+println patch.getDeltas().size()
+
+patch.getDeltas().each {
+    println it
+}
+
+assert patch.getDeltas().size() == 0
