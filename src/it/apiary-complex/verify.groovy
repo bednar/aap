e12dc0df5b762b305051acbb17a98cb02cfa31ad
bednar@borek-laptop <jakub.bednar@gmail.com>
2013-11-04 16:39:37 +0100

    * Print Files

--- src/it/apiary-complex/verify.groovy
+++ src/it/apiary-complex/verify.groovy
@@ -3,8 +3,14 @@ import difflib.*
 apiaryExpected = new File(basedir, "ApiaryExpected.md")
 assert apiaryExpected.exists()
 
+println '\n Expected: '
+println apiaryExpected.getText()
+
 apiaryResult = new File(basedir, "target/generated/apiary/Apiary.md")
 assert apiaryResult.exists()
+
+println '\n Result: '
+println apiaryResult.getText()
 
 Patch patch = DiffUtils.diff(apiaryExpected.readLines(), apiaryResult.readLines())
 
